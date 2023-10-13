6fa38b7bd8f11d462409df38d9c1ec82cfbbe836
MaiT-027 <maitmus@gmail.com>
2023-10-13 22:30:08 +0900

    Database 연결

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import db from "./database/auth_db";
 
 const app = express();
+db.connect();
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (_req, res) => {
-  res.json({ result: "OK" });
+  const sql = "SHOW TABLES";
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      console.error(err);
+      return res.json(rows);
+    }
+    res.json({ connect: "OK" });
+  });
 });
 
 app.listen(3000, () => {
