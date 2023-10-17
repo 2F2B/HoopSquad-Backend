ca3516fe6161d78f3c8b2dae28694aca7cb50fe4
MaiT-027 <maitmus@gmail.com>
2023-10-18 02:11:59 +0900

    농구장 조회/추가 구현

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const client_1 = require("@prisma/client");
+const court_1 = require("./court/court");
 const app = (0, express_1.default)();
 const prisma = new client_1.PrismaClient();
 app.use((0, cors_1.default)());
@@ -29,6 +30,25 @@ app.get("/", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
         res.json(err);
         return console.error(err);
     }
+}));
+app.get("/api/court", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let result;
+    if (req.query.id) {
+        //쿼리 문자열 있음
+        if (typeof req.query.id === "string") {
+            result = yield (0, court_1.getCourt)(parseInt(req.query.id));
+            res.json(result);
+        }
+    }
+    else {
+        //쿼리 문자열 없음
+        result = yield (0, court_1.getCourt)();
+        res.json(result);
+    }
+}));
+app.post("/api/court/add", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield (0, court_1.addCourt)(req.body);
+    res.json(result);
 }));
 app.listen(3000, () => {
     console.log("Server started on Port 3000");
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { PrismaClient } from "@prisma/client";
+import { getCourt, addCourt } from "./court/court";
 
 const app = express();
 const prisma = new PrismaClient();
@@ -17,6 +18,26 @@ app.get("/", async (_req, res) => {
     res.json(err);
     return console.error(err);
   }
+});
+
+app.get("/api/court", async (req, res) => {
+  let result;
+  if (req.query.id) {
+    //쿼리 문자열 있음
+    if (typeof req.query.id === "string") {
+      result = await getCourt(parseInt(req.query.id));
+      res.json(result);
+    }
+  } else {
+    //쿼리 문자열 없음
+    result = await getCourt();
+    res.json(result);
+  }
+});
+
+app.post("/api/court/add", async (req, res) => {
+  const result = await addCourt(req.body);
+  res.json(result);
 });
 
 app.listen(3000, () => {
