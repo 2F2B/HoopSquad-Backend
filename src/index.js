f90cf8036162631e566340ec02e87e8e92ea0506
MaiT-027 <maitmus@gmail.com>
2023-10-19 00:23:45 +0900

    .

--- src/index.js
+++ src/index.js
@@ -1,42 +1,16 @@
 "use strict";
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator["throw"](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
@@ -50,17 +24,16 @@ app.use("/auth", authRouter_1.default);
 app.use("/court", courtRouter_1.default);
 app.use((0, cors_1.default)());
 app.use(body_parser_1.default.json());
-app.get("/", (_req, res) =>
-  __awaiter(void 0, void 0, void 0, function* () {
+app.get("/", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-      yield prisma.$queryRaw`SHOW TABLES`;
-      res.json({ connect: "OK" });
-    } catch (err) {
-      res.json(err);
-      return console.error(err);
+        yield prisma.$queryRaw `SHOW TABLES`;
+        res.json({ connect: "OK" });
     }
-  })
-);
+    catch (err) {
+        res.json(err);
+        return console.error(err);
+    }
+}));
 app.listen(3000, () => {
-  console.log("Server started on Port 3000");
+    console.log("Server started on Port 3000");
 });
