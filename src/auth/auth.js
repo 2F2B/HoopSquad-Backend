a5f0f0efa80064a0ea70f68cf5ebb8a1eeb6472a
MaiT-027 <maitmus@gmail.com>
2023-10-18 22:30:09 +0900

    Kakao로 로그인 구현 완료

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,6 +1,33 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-function Response() {
-    return { response: "OK" };
+exports.LoginKakao = void 0;
+const axios_1 = __importDefault(require("axios"));
+const apiKey_1 = require("../apiKey");
+function LoginKakao(code) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const result = yield axios_1.default.post("https://kauth.kakao.com/oauth/token", {
+            grant_type: "authorization_code",
+            client_id: apiKey_1.kakaoAPIKey,
+            redirect_uri: "http://ec2-52-79-227-4.ap-northeast-2.compute.amazonaws.com/auth/register",
+            code: code,
+        }, {
+            headers: {
+                "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+            },
+        });
+        return result.data;
+    });
 }
-exports.default = Response;
+exports.LoginKakao = LoginKakao;
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,12 +1,28 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const auth_1 = __importDefault(require("../auth/auth"));
+const auth_1 = require("../auth/auth");
 const authRouter = express_1.default.Router();
-authRouter.use("/", (req, res) => {
-    res.send((0, auth_1.default)());
-});
+authRouter.get("/register", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const data = yield (0, auth_1.LoginKakao)(req.query.code);
+        res.send(data);
+    }
+    catch (err) {
+        res.status(400);
+        res.send({ result: "error" });
+    }
+}));
 exports.default = authRouter;
