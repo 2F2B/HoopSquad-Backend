ef92bbc77658ff10d9cc644bc8ae4d01b1d18262
MaiT-027 <maitmus@gmail.com>
2023-10-20 00:26:47 +0900

    액세스 토큰 유효성 검사 구현

--- src/auth/kakaoAuth.js
+++ src/auth/kakaoAuth.js
@@ -15,19 +15,69 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.LoginKakao = void 0;
 const axios_1 = __importDefault(require("axios"));
 const apiKey_1 = require("../apiKey");
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
 function LoginKakao(code) {
     return __awaiter(this, void 0, void 0, function* () {
-        const result = yield axios_1.default.post("https://kauth.kakao.com/oauth/token", {
+        const token = yield axios_1.default.post("https://kauth.kakao.com/oauth/token", {
             grant_type: "authorization_code",
             client_id: apiKey_1.kakaoAPIKey,
-            redirect_uri: "http://ec2-52-79-227-4.ap-northeast-2.compute.amazonaws.com/auth/register/kakao",
+            redirect_uri: "http://localhost:3000/auth/register/kakao",
             code: code,
         }, {
             headers: {
                 "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
             },
         });
-        return result.data;
+        const user = yield axios_1.default.get("https://kapi.kakao.com/v2/user/me", {
+            headers: {
+                Authorization: `Bearer ${token.data.access_token}`,
+                "Content-Type": "	Content-type: application/x-www-form-urlencoded;charset=utf-8",
+            },
+        });
+        const isUserExist = yield prisma.oAuthToken.findFirst({
+            where: {
+                Auth_id: user.data.id.toString(),
+            },
+        });
+        const currentTimeInSecond = Math.floor(Date.now() / 1000);
+        if (!isUserExist) {
+            //유저 정보가 DB에 없음
+            const expires_in = token.data.expires_in;
+            const refresh_token_expires_in = token.data
+                .refresh_token_expires_in;
+            const result = yield prisma.user.create({
+                //유저 정보를 DB에 추가
+                data: {
+                    Name: user.data.properties.nickname,
+                    OAuthToken: {
+                        create: {
+                            Auth_id: user.data.id.toString(),
+                            AccessToken: token.data.access_token,
+                            RefreshToken: token.data.refresh_token,
+                            AToken_Expires: expires_in,
+                            RToken_Expires: refresh_token_expires_in,
+                            CreatedAt: currentTimeInSecond.toString(),
+                        },
+                    },
+                },
+                include: {
+                    OAuthToken: true,
+                },
+            });
+            return result;
+        }
+        else {
+            //유저 정보가 DB에 있음
+            if (isUserExist.AToken_Expires + parseInt(isUserExist.CreatedAt) <
+                currentTimeInSecond) {
+                //토큰 만료
+                return { token: "Expired" };
+                //TODO: 리프레시 토큰 발급
+            }
+            else
+                return isUserExist; //TODO: 로그인 승인
+        }
     });
 }
 exports.LoginKakao = LoginKakao;
--- src/auth/kakaoAuth.ts
+++ src/auth/kakaoAuth.ts
@@ -1,14 +1,16 @@
 import axios from "axios";
 import { kakaoAPIKey } from "../apiKey";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 async function LoginKakao(code: any) {
-  const result = await axios.post(
+  const token = await axios.post(
     "https://kauth.kakao.com/oauth/token",
     {
       grant_type: "authorization_code",
       client_id: kakaoAPIKey,
-      redirect_uri:
-        "http://ec2-52-79-227-4.ap-northeast-2.compute.amazonaws.com/auth/register/kakao",
+      redirect_uri: "http://localhost:3000/auth/register/kakao",
       code: code,
     },
     {
@@ -18,7 +20,57 @@ async function LoginKakao(code: any) {
     },
   );
 
-  return result.data;
+  const user = await axios.get("https://kapi.kakao.com/v2/user/me", {
+    headers: {
+      Authorization: `Bearer ${token.data.access_token}`,
+      "Content-Type":
+        "	Content-type: application/x-www-form-urlencoded;charset=utf-8",
+    },
+  });
+
+  const isUserExist = await prisma.oAuthToken.findFirst({
+    where: {
+      Auth_id: user.data.id.toString(),
+    },
+  });
+  const currentTimeInSecond = Math.floor(Date.now() / 1000);
+  if (!isUserExist) {
+    //유저 정보가 DB에 없음
+    const expires_in = token.data.expires_in as number;
+    const refresh_token_expires_in = token.data
+      .refresh_token_expires_in as number;
+
+    const result = await prisma.user.create({
+      //유저 정보를 DB에 추가
+      data: {
+        Name: user.data.properties.nickname,
+        OAuthToken: {
+          create: {
+            Auth_id: user.data.id.toString(),
+            AccessToken: token.data.access_token,
+            RefreshToken: token.data.refresh_token,
+            AToken_Expires: expires_in,
+            RToken_Expires: refresh_token_expires_in,
+            CreatedAt: currentTimeInSecond.toString(),
+          },
+        },
+      },
+      include: {
+        OAuthToken: true,
+      },
+    });
+    return result;
+  } else {
+    //유저 정보가 DB에 있음
+    if (
+      isUserExist.AToken_Expires + parseInt(isUserExist.CreatedAt) <
+      currentTimeInSecond
+    ) {
+      //토큰 만료
+      return { token: "Expired" };
+      //TODO: 리프레시 토큰 발급
+    } else return isUserExist; //TODO: 로그인 승인
+  }
 }
 
 export { LoginKakao };
