72d655d820fe0453e693858d2f8bcfd71fee1315
ipmango <103362844+iipmango@users.noreply.github.com>
2023-10-20 18:23:16 +0900

    Add files via upload
    GAuthkey 업로드

---
+++ src/GAuthKeys.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.gUserInfoUri = exports.gToken_uri = exports.gSignup_Redirect_uri = exports.gLoginRedirectUri = exports.gClientSecret = exports.gClientId = void 0;
+const gClientId = "872643862888-0fooeuci523beeld4c18ocnkvujmbgq2.apps.googleusercontent.com";
+exports.gClientId = gClientId;
+const gClientSecret = "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";
+exports.gClientSecret = gClientSecret;
+const gSignup_Redirect_uri = "http://localhost:3000/auth/register/redirect";
+exports.gSignup_Redirect_uri = gSignup_Redirect_uri;
+const gLoginRedirectUri = "http://localhost:3000/auth/login/redirect";
+exports.gLoginRedirectUri = gLoginRedirectUri;
+const gToken_uri = "https://oauth2.googleapis.com/token";
+exports.gToken_uri = gToken_uri;
+const gUserInfoUri = "https://www.googleapis.com/oauth2/v2/userinfo";
+exports.gUserInfoUri = gUserInfoUri;
