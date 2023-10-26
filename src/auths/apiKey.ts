b53e7c0696de631dda668a4349c0aa16c09d05f8
ipmango <andykwon36@gmail.com>
2023-10-27 00:13:17 +0900

    커밋

---
+++ src/auths/apiKey.ts
@@ -0,0 +1,4 @@
+const gMap = "AIzaSyBhFzWbHsYZ8Cntn79RJgXrG9XDXVfj4Bk";
+const kakaoAPIKey = "10e7f19b2d7eb9d4b9dd0fe3568e74bb";
+const RESTAPIKey = "0c2ef36886762ddaf02c8778d2930772";
+export { gMap, kakaoAPIKey, RESTAPIKey };
