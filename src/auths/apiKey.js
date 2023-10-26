93da59f2bf2008dc5a844f2135d5bdebdc543e8e
ipmango <103362844+iipmango@users.noreply.github.com>
2023-10-27 00:53:15 +0900

    Delete src/auths directory

--- src/auths/apiKey.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RESTAPIKey = exports.kakaoAPIKey = exports.gMap = void 0;
-const gMap = "AIzaSyBhFzWbHsYZ8Cntn79RJgXrG9XDXVfj4Bk";
-exports.gMap = gMap;
-const kakaoAPIKey = "10e7f19b2d7eb9d4b9dd0fe3568e74bb";
-exports.kakaoAPIKey = kakaoAPIKey;
-const RESTAPIKey = "0c2ef36886762ddaf02c8778d2930772";
-exports.RESTAPIKey = RESTAPIKey;
--- src/auths/apiKey.ts
+++
@@ -1,4 +0,0 @@
-const gMap = "AIzaSyBhFzWbHsYZ8Cntn79RJgXrG9XDXVfj4Bk";
-const kakaoAPIKey = "10e7f19b2d7eb9d4b9dd0fe3568e74bb";
-const RESTAPIKey = "0c2ef36886762ddaf02c8778d2930772";
-export { gMap, kakaoAPIKey, RESTAPIKey };
