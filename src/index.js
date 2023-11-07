7c8c8fa69ae0171ff5892091f50be628265d0d32
ipmango <andykwon36@gmail.com>
2023-11-07 14:20:55 +0900

    match 브랜치 생성 및 개발 시작

--- src/index.js
+++ src/index.js
@@ -23,10 +23,12 @@ const httpServer = http_1.default.createServer(app);
 const authRouter = require("./routes/authRouter");
 const courtRouter = require("./routes/courtRouter");
 const { chatRouter, socketIOHandler } = require("./routes/chatRouter");
+const matchRouter = require("./routes/matchRouter");
 app.use("/auth", authRouter);
 app.use("/court", courtRouter);
 app.use("/chat", chatRouter);
 socketIOHandler(httpServer);
+app.use("/match", matchRouter);
 app.get("/", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         res.json({ connect: "OK" });
--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,14 @@ const httpServer = http.createServer(app);
 const authRouter = require("./routes/authRouter");
 const courtRouter = require("./routes/courtRouter");
 const { chatRouter, socketIOHandler } = require("./routes/chatRouter");
+const matchRouter = require("./routes/matchRouter");
 
 app.use("/auth", authRouter);
 app.use("/court", courtRouter);
 app.use("/chat", chatRouter);
 
 socketIOHandler(httpServer);
+app.use("/match", matchRouter);
 
 app.get("/", async (_req, res) => {
   try {
