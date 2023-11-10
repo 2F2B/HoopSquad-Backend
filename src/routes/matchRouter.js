a36cf2375fe5f02a71e09032768b5770b34163d8
ipmango <andykwon36@gmail.com>
2023-11-10 09:13:10 +0900

    매칭 기능 테스트 및 코드 상세 수정 중

--- src/routes/matchRouter.js
+++ src/routes/matchRouter.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,45 +15,49 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const match_1 = require("../match/match");
 const matchRouter = express_1.default.Router();
-matchRouter.get("/", (req, res) => {
+matchRouter.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     // 전체 게시글 조회
     try {
-        const result = (0, match_1.AllMatch)(req);
+        const result = yield (0, match_1.AllMatch)(req);
+        res.status(200);
         res.send(result);
     }
     catch (err) {
         console.log(err);
         res.send({ result: "error" });
     }
-});
-matchRouter.get("/filter", (req, res) => {
+}));
+matchRouter.get("/filter", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const add = (0, match_1.MatchFilter)(req);
+        const add = yield (0, match_1.MatchFilter)(req);
+        res.status(200);
         res.send(add);
     }
     catch (err) {
         console.log(err);
         res.send({ result: "error" });
     }
-});
-matchRouter.get("/id", (req, res) => {
+}));
+matchRouter.get("/info", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const add = (0, match_1.MatchInfo)(req);
+        const add = yield (0, match_1.MatchInfo)(req);
+        res.status(200);
         res.send(add);
     }
     catch (err) {
         console.log(err);
         res.send({ result: "error" });
     }
-});
-matchRouter.get("/add", (req, res) => {
+}));
+matchRouter.get("/add", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const add = (0, match_1.AddMatch)(req);
+        const add = yield (0, match_1.AddMatch)(req);
+        res.status(201);
         res.send(add);
     }
     catch (err) {
         console.log(err);
         res.send({ result: "error" });
     }
-});
+}));
 module.exports = matchRouter;
