fd0f0d27b6ee86fefb5a8c6582fe90cefcc09cb9
ipmango <andykwon36@gmail.com>
2023-11-14 00:01:58 +0900

    profileRouter 구현

--- src/profile/User.ts
+++ src/profile/User.ts
@@ -6,51 +6,12 @@ import { ParsedQs } from "qs";
 const prisma = new PrismaClient();
 
 async function getUserProfile(userId: number) {
-  const Profile = await prisma.user.findFirst({
+  const Profile = await prisma.profile.findFirst({
     where: {
       User_id: userId,
     },
-    select: {
-      Name: true,
-      Profile: true,
-    },
   });
-  return { ...Profile?.Profile, Name: Profile?.Name };
+  return Profile;
 }
 
-async function setUserProfile(
-  req: Request<{}, any, any, ParsedQs, Record<string, any>>,
-) {
-  const isUser = await prisma.oAuthToken.findFirst({
-    where: {
-      AccessToken: req.body.access_token,
-    },
-    select: {
-      User_id: true,
-    },
-  });
-
-  if (!isUser) throw new Error("User Not Exists");
-
-  const profile = await prisma.profile.create({
-    data: {
-      User: { connect: { User_id: isUser.User_id } },
-      Height: req.body.Height,
-      Age: req.body.Age,
-      Position: req.body.Position,
-      Grade: req.body.Grade,
-      Introduce: req.body.Introduce,
-      Location: req.body.Location,
-      GameType: {
-        create: {
-          OneOnOne: req.body.One,
-          ThreeOnThree: req.body.Three,
-          FiveOnFive: req.body.Five,
-        },
-      },
-    },
-  });
-  return profile;
-}
-
-export { getUserProfile, setUserProfile };
+export { getUserProfile };
