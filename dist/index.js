cc6be7b74d0bc265fd29e8f8ebf931973a766187
ipmango <andykwon36@gmail.com>
2023-11-15 10:38:07 +0900

    매칭 이미지 삽입 관련 코드 작성, 테스트 필요

--- dist/index.js
+++ dist/index.js
@@ -1,26 +1,50 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const http_1 = __importDefault(require("http"));
-const multipart = require("connect-multiparty");
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(body_parser_1.default.json());
-app.use(multipart());
 const httpServer = http_1.default.createServer(app);
 const authRouter = require("./routes/authRouter");
 const courtRouter = require("./routes/courtRouter");
@@ -31,28 +55,30 @@ app.use("/auth", authRouter);
 app.use("/court", courtRouter);
 app.use("/match", matchRouter);
 app.use("/profile", profileRouter);
-app.use(body_parser_1.default.raw({
+app.use(
+  body_parser_1.default.raw({
     type: "image/jpeg",
     limit: "10mb",
-}));
+  }),
+);
 try {
-    socketIOHandler(httpServer);
+  socketIOHandler(httpServer);
+} catch (err) {
+  if (err instanceof Error) {
+    console.error(err);
+  }
 }
-catch (err) {
-    if (err instanceof Error) {
-        console.error(err);
-    }
-}
-app.get("/", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get("/", (_req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     try {
-        res.json({ connect: "OK" });
+      res.json({ connect: "OK" });
+    } catch (err) {
+      res.json(err);
+      return console.error(err);
     }
-    catch (err) {
-        res.json(err);
-        return console.error(err);
-    }
-}));
+  }),
+);
 httpServer.listen(3000, () => {
-    console.log("Server started on Port 3000");
+  console.log("Server started on Port 3000");
 });
 //# sourceMappingURL=index.js.map
