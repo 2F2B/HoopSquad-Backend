1c7f0d29729e3103d2a3227190c6eee5632c4b99
MaiT <maitmus@gmail.com>
2023-11-16 16:03:57 +0900

    채팅 코드 Fix

--- src/routes/chatRouter.ts
+++ src/routes/chatRouter.ts
@@ -239,7 +239,7 @@ const socketIOHandler = (
         }
 
         console.log(data);
-        socket.to(currentRoom).emit("receive", {
+        socket.to(currentRoom).emit("sendCallback", {
           nickname: socket["nickname"],
           ...data,
           createdAt: Date.now(),
