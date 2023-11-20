2b0f1d049eaa033e567ed55b2358b148b0b318f7
ipmango <andykwon36@gmail.com>
2023-11-20 14:38:35 +0900

    routes 응답 수정

--- src/routes/matchRouter.ts
+++ src/routes/matchRouter.ts
@@ -9,27 +9,6 @@ const upload = multer({ storage: storage });
 
 const matchRouter = express.Router();
 
-matchRouter.post("/", upload.single("Image"), async (req, res) => {
-  try {
-    // console.log(image);
-    if (!req.body) throw new Error("Body Not Exists");
-    const add = await AddMatch(req);
-    res.status(201);
-    if (req.file) {
-      storage._removeFile(req, req.file, (err) => {
-        if (err) throw new Error("File Deletion Failed");
-      });
-    }
-    res.send(add);
-  } catch (err) {
-    if (err instanceof Error) {
-      res.status(400);
-      console.log(err);
-      res.send({ error: err.message });
-    }
-  }
-});
-
 matchRouter.get("/", async (req, res) => {
   try {
     let result;
@@ -46,48 +25,26 @@ matchRouter.get("/", async (req, res) => {
     }
   }
 });
-// matchRouter.post("/", async (req, res) => {
-//   // 전체 게시글 조회
-//   try {
-//     console.log(req.body);
-//     const result = await AllMatch(req);
-//     res.status(200);
-//     res.send(result);
-//   } catch (err) {
-//     console.log(err);
-//     res.send({ result: "error" });
-//   }
-// });
 
-// matchRouter.get("/", async (req, res) => {
-//   try {
-//     const add = await MatchInfo(req);
-//     res.status(200);
-//     res.send(add);
-//   } catch (err) {
-//     console.log(err);
-//     res.send({ result: "error" });
-//   }
-// });
-
-// matchRouter.post("/add", upload.single("Image"), async (req, res) => {
-//   try {
-//     // console.log(image);
-//     const add = await AddMatch(req);
-//     res.status(201);
-//     if (req.file) {
-//       storage._removeFile(req, req.file, (err) => {
-//         if (err) throw new Error("File Deletion Failed");
-//       });
-//     }
-//     res.send(add);
-//   } catch (err) {
-//     if (err instanceof Error) {
-//       res.status(400);
-//       console.log(err);
-//       res.send({ error: err.message });
-//     }
-//   }
-// });
+matchRouter.post("/", upload.single("Image"), async (req, res) => {
+  try {
+    // console.log(image);
+    if (!req.body) throw new Error("Body Not Exists");
+    const add = await AddMatch(req);
+    res.status(201);
+    if (req.file) {
+      storage._removeFile(req, req.file, (err) => {
+        if (err) throw new Error("File Deletion Failed");
+      });
+    }
+    res.send(add);
+  } catch (err) {
+    if (err instanceof Error) {
+      res.status(401);
+      console.log(err);
+      res.send({ error: err.message });
+    }
+  }
+});
 
 module.exports = matchRouter;
