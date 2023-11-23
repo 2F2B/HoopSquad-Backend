5961584deac7d56f8cc8ccdb1e0f6b640145e3b0
MaiT <maitmus@gmail.com>
2023-11-23 20:16:59 +0900

    fixed the hardcoded errors

--- src/routes/teamRouter.ts
+++ src/routes/teamRouter.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import getTeam from "../team/teamSearch";
+import { TeamNotFoundError } from "../team/error";
 
 const teamRouter = express.Router();
 
@@ -20,13 +21,14 @@ teamRouter.get("/", async (_req, res) => {
 teamRouter.get("/:id", async (req, res) => {
   try {
     const result = await getTeam(+req.params.id);
-    res.status(200);
     res.json(result);
   } catch (err) {
-    if (err instanceof Error) {
+    if (err instanceof TeamNotFoundError) {
       console.error(err);
-      res.status(400);
-      res.json({ error: err.message });
+      res.status(err.statusCode).json({ error: err.message });
+    } else if (err instanceof Error) {
+      console.error(err);
+      res.status(400).json({ error: err.message });
     }
   }
 });
