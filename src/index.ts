86c06996f5cc14e3295bae83f722ee16cda4e637
MaiT <maitmus@gmail.com>
2023-12-06 22:43:32 +0900

    minor fixes

--- src/index.ts
+++ src/index.ts
@@ -56,7 +56,7 @@ const chatServer = new SocketIO.Server(httpServer, {
 const notificationServer = chatServer.of("/notification");
 
 chatServerHandler(chatServer, notificationServer);
-notificationServerHandler(notificationServer, chatServer);
+notificationServerHandler(notificationServer);
 
 app.get("/", async (_req, res) => {
   try {
