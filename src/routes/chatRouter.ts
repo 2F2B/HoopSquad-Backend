997b56c70deef167153f4d112283db7ebd37ce2f
MaiT <maitmus@gmail.com>
2023-12-06 19:15:20 +0900

    로직 구현 및 수정

--- src/routes/chatRouter.ts
+++ src/routes/chatRouter.ts
@@ -46,6 +46,8 @@ type joinRoomType = {
   postingId: number;
 };
 
+const expoPushTokens = new Map<string, string>();
+
 const chatServerHandler = (
   io: SocketIO.Server,
   notificationServer: SocketIO.Namespace,
@@ -149,14 +151,19 @@ const chatServerHandler = (
           getRoomName(postingId),
         );
 
-        socket.to(getRoomName(postingId)).emit("updateChatRoom", {
-          nickname: nickname,
-          lastChatMessage: payload,
-          lastChatTime: currentTimestamp,
-          postingId: postingId,
-          postingTitle: post.Title,
-          entireMessagesAmount: entireMessagesAmount.length,
-          test: "test",
+        socketsInRooms?.forEach(async (socketId) => {
+          if (
+            socketId != socket.id &&
+            (await checkUserOffline(io, socket.id))
+          ) {
+            notificationServer.emit(
+              "newMessageNotification",
+              expoPushTokens.get(socketId)!!,
+              nickname,
+              post.Title,
+              payload,
+            );
+          }
         });
 
         // if (await checkUserOffline(io, +hostId)) {
